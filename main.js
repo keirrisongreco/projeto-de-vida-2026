7546ff30fe3885b7a40ac23dc9540a18e1332672
keirrisongreco <keirrison.greco@escola.pr.gov.br>
2026-05-25 08:04:56 -0300

    salvar

--- main.js
+++ main.js
@@ -15,10 +15,21 @@ for (let i = 0; i < botoes.length; i++) {
     }
 }
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2026-19-9T00:00:00");
-let tempoAtual = new Date();
+const tempoObjetivo1 = new Date("2026-10-9T00:00:00");
+const tempoObjetivo2 = new Date("2026-12-30T00:00:00");
+const tempoObjetivo3 = new Date("2026-6-25T00:00:00");
+const tempoObjetivo4 = new Date("2027-12-30T00:00:00");
 
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4]
+
+
+function atualizaCronometro(){
+for(let i=0;i<contadores.length;i++){
+contadores[i].textContent = calculaTempo(tempos[i]);
+}
+}
+
+atualizaCronometro();
 
 function calculaTempo(tempoObjetivo1) {
     let tempoAtual = new Date();

70b7bb991029742ada208514fe60bcb809d708f8
keirrisongreco <keirrison.greco@escola.pr.gov.br>
2026-06-08 08:00:08 -0300

    aula 8

--- main.js
+++ main.js
@@ -23,11 +23,7 @@ const tempoObjetivo4 = new Date("2027-12-30T00:00:00");
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4]
 
 
-function atualizaCronometro(){
-for(let i=0;i<contadores.length;i++){
-contadores[i].textContent = calculaTempo(tempos[i]);
-}
-}
+
 
 atualizaCronometro();
 
@@ -44,14 +40,27 @@ function calculaTempo(tempoObjetivo1) {
     minutos %= 60;
     horas %= 24;
 
-    return (
-        dias +
-        " dias " +
-        horas +
-        " horas " +
-        minutos +
-        " minutos " +
-        segundos +
-        " segundos"
-    );
+    if (tempoFinal > 0) {
+  return [dias, horas, minutos, segundos];
+} else {
+  return [0, 0, 0, 0];
 }
+}
+
+function atualizaCronometro() {
+  document.getElementById("dias0").textContent = calculaTempo(tempos[1])[0];
+  document.getElementById("horas0").textContent = calculaTempo(tempos[1])[1];
+  document.getElementById("min0").textContent = calculaTempo(tempos[1])[2];
+  document.getElementById("seg0").textContent = calculaTempo(tempos[1])[3];
+  for (let i = 0; i < contadores.length; i++) {
+    //contadores[i].textContent = calculaTempo(tempos[i]);
+  }
+}
+
+
+function comecaCronometro(){
+    atualizaCronometro();
+    setInterval(atualizaCronometro, 1000);
+}
+
+comecaCronometro();
